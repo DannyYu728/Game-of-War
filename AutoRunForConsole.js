9decc916672d858bb8f6f4426f1ac4dfc143454d
Danny Yu <tiramisu@Dannys-MBP.home>
2022-09-17 19:45:09 -0400

    add HTML & CSS elements, also added a new JS file to autorun in console

--- AutoRunForConsole.js
+++ AutoRunForConsole.js
@@ -43,13 +43,6 @@ function splitDeck() {
   player2Deck = shuffledDeck.deck
 }
 splitDeck()
-//turn this off to run in node
-function results(text) {
-  const node = document.createElement("li");
-  const textnode = document.createTextNode(text);
-  node.appendChild(textnode);
-  document.getElementById("Results").appendChild(node);
-}
 //Game's logic below, might add some variable to make it neater
 //table is where the players compare their cards
 //war is the array to store the 6 cards dealed from a war and 2 from the table
@@ -59,12 +52,10 @@ function gameOfWar() {
   while (player1Deck.length !== 0 && player2Deck.length !== 0) {
     table.unshift(player1Deck.shift(), player2Deck.shift())
     let player1points = table[0].points;
-    let player2points = table[1].points;
+    let player2points= table[1].points;
     let p1CardValue = table[0].values;
     let p2CardValue = table[1].values;
     console.log(`player 1 draws ${p1CardValue}, player 2 draws ${p2CardValue}`);
-    //turn off results to run in node because DOM doesnt work
-    results(`player 1 draws ${p1CardValue}, player 2 draws ${p2CardValue}`)
     //if player 1 don't have enough cards for war, it adds all the remaining cards except 1 for another more round.
     if (player1points === player2points && player1Deck.length <= 3) {
       war.unshift(player1Deck.length - 1);
@@ -72,8 +63,6 @@ function gameOfWar() {
       player1Deck.splice(0, player1Deck.length - 1);
       player2Deck.splice(0, 3);
       console.log(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`);
-      //turn off results to run in node because DOM doesnt work
-      results(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`)
       //if player 2 don't have enough cards for war, it adds all the remaining cards except 1 for another more round.
     } else if (player1points === player2points && player2Deck.length <= 3) {
       war.unshift(player1Deck[0], player1Deck[1], player1Deck[2]);
@@ -81,22 +70,16 @@ function gameOfWar() {
       player1Deck.splice(0, 3);
       player2Deck.splice(0, player2Deck.length - 1);
       console.log(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`);
-      //turn off results to run in node because DOM doesnt work
-      results(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`)
       //regular war if both players have enough cards
     } else if (player1points === player2points) {
+      console.log(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`)
       war.unshift(player1Deck[0], player1Deck[1], player1Deck[2]);
       war.unshift(player2Deck[0], player2Deck[1], player2Deck[2]);
       player1Deck.splice(0, 3);
       player2Deck.splice(0, 3);
-      console.log(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`)
-      //turn off results to run in node because DOM doesnt work
-      results(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`)
       //player 1 wins the round, the cards are push in from the back in reverse or else there will be an infinite loop
     } else if (player1points > player2points) {
       console.log(`Player 1 wins this round. Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining and takes ${war.length} from the war and ${table.length} cards from the table`);
-      //turn off results to run in node because DOM doesnt work
-      results(`Player 1 wins this round. Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining and takes ${war.length} from the war and ${table.length} cards from the table`)
       war.reverse();
       player1Deck.push(...war);
       war.length = 0;
@@ -106,8 +89,6 @@ function gameOfWar() {
       //player 2 wins the round. the cards go in from the front of the array as normal.
     } else {
       console.log(`Player 2 wins this round. Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining and takes ${war.length} from the war and ${table.length} cards from the table`);
-      //turn off results to run in node because DOM doesnt work
-      results(`Player 2 wins this round. Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining and takes ${war.length} from the war and ${table.length} cards from the table`)
       player2Deck.push(...war);
       war.length = 0;
       player2Deck.push(...table);
@@ -117,19 +98,9 @@ function gameOfWar() {
   //announce the winner
   if (player1Deck == 0) {
     console.log('Player 2 wins the game');
-    //turn off below to run in node because DOM doesnt work
-    document.getElementById("final").innerHTML += 'Player 2 wins the game'
   } else {
     console.log('Player 1 wins the game');
-    //turn off below to run in node because DOM doesnt work
-    document.getElementById("final").innerText += 'Player 1 wins the game'
   }
 }
-// restart the game button
-function resetting() {
-  document.location.reload();
-}
-//start the game function for node/console/debug otherwise use start game button on site
-// gameOfWar()
-
-
+//start the game function
+gameOfWar()
