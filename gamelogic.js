7ad1b92737c40d6b7a89103f7f038528a853b52e
Danny Yu <tiramisu@Dannys-MBP.home>
2022-09-17 16:33:58 -0400

    file created

---
+++ gamelogic.js
@@ -0,0 +1,8 @@
+//this class create an object for each card
+class Card {
+  constructor(suits, values, points) {
+    this.suits = suits;
+    this.values = values;
+    this.points = points;
+  }
+}
