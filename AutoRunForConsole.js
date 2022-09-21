62d0ca9a160b65fbf40831154ed5f62d2a25351a
Danny Yu <tiramisu@Dannys-MacBook-Pro.local>
2022-09-21 09:19:17 -0400

    put everything in Setup Class

--- AutoRunForConsole.js
+++ AutoRunForConsole.js
@@ -13,6 +13,10 @@ class Setup {
     this.deck = [];
     this.createDeck();
     this.shuffler();
+    this.player1Deck = []
+    this.player2Deck = []
+    this.splitDeck();
+    this.gameOfWar()
   }
   suits = ["Dia", "Club", "Heart", "Spade"];
   values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
@@ -35,72 +39,73 @@ class Setup {
       this.deck[randomGeneratedIndex2] = tempSavedItem;
     }
   }
-}
-//below is a quick function to split the newly shuffle deck to both players.
-function splitDeck() {
-  let shuffledDeck = new Setup()
-  player1Deck = shuffledDeck.deck.splice(0, 26)
-  player2Deck = shuffledDeck.deck
-}
-splitDeck()
-//Game's logic below, might add some variable to make it neater
-//table is where the players compare their cards
-//war is the array to store the 6 cards dealed from a war and 2 from the table
-function gameOfWar() {
-  let table = []
-  let war = []
-  while (player1Deck.length !== 0 && player2Deck.length !== 0) {
-    table.unshift(player1Deck.shift(), player2Deck.shift())
-    let player1points = table[0].points;
-    let player2points= table[1].points;
-    let p1CardValue = table[0].values;
-    let p2CardValue = table[1].values;
-    console.log(`player 1 draws ${p1CardValue}, player 2 draws ${p2CardValue}`);
-    //if player 1 don't have enough cards for war, it adds all the remaining cards except 1 for another more round.
-    if (player1points === player2points && player1Deck.length <= 3) {
-      war.unshift(player1Deck.length - 1);
-      war.unshift(player2Deck[0], player2Deck[1], player2Deck[2]);
-      player1Deck.splice(0, player1Deck.length - 1);
-      player2Deck.splice(0, 3);
-      console.log(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`);
-      //if player 2 don't have enough cards for war, it adds all the remaining cards except 1 for another more round.
-    } else if (player1points === player2points && player2Deck.length <= 3) {
-      war.unshift(player1Deck[0], player1Deck[1], player1Deck[2]);
-      war.unshift(player2Deck.length - 1);
-      player1Deck.splice(0, 3);
-      player2Deck.splice(0, player2Deck.length - 1);
-      console.log(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`);
-      //regular war if both players have enough cards
-    } else if (player1points === player2points) {
-      console.log(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`)
-      war.unshift(player1Deck[0], player1Deck[1], player1Deck[2]);
-      war.unshift(player2Deck[0], player2Deck[1], player2Deck[2]);
-      player1Deck.splice(0, 3);
-      player2Deck.splice(0, 3);
-      //player 1 wins the round, the cards are push in from the back in reverse or else there will be an infinite loop
-    } else if (player1points > player2points) {
-      console.log(`Player 1 wins this round. Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining and takes ${war.length} from the war and ${table.length} cards from the table`);
-      war.reverse();
-      player1Deck.push(...war);
-      war.length = 0;
-      table.reverse();
-      player1Deck.push(...table);
-      table.length = 0;
-      //player 2 wins the round. the cards go in from the front of the array as normal.
+  //below is a quick function to split the newly shuffle deck to both players.
+  splitDeck() {
+    this.player1Deck = this.deck.splice(0, 26)
+    this.player2Deck = this.deck
+  }
+  //Game's logic below, might add some variable to make it neater
+  //table is where the players compare their cards
+  //war is the array to store the 6 cards dealed from a war and 2 from the table
+  gameOfWar() {
+    let table = []
+    let war = []
+    let player1Deck = this.player1Deck
+    let player2Deck = this.player2Deck
+    while (player1Deck.length !== 0 && player2Deck.length !== 0) {
+      table.unshift(player1Deck.shift(), player2Deck.shift())
+      let player1points = table[0].points;
+      let player2points = table[1].points;
+      let p1CardValue = table[0].values;
+      let p2CardValue = table[1].values;
+      console.log(`player 1 draws ${p1CardValue}, player 2 draws ${p2CardValue}`);
+      //if player 1 don't have enough cards for war, it adds all the remaining cards except 1 for another more round.
+      if (player1points === player2points && player1Deck.length <= 3) {
+        war.unshift(player1Deck.length - 1);
+        war.unshift(player2Deck[0], player2Deck[1], player2Deck[2]);
+        player1Deck.splice(0, player1Deck.length - 1);
+        player2Deck.splice(0, 3);
+        console.log(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`);
+        //if player 2 don't have enough cards for war, it adds all the remaining cards except 1 for another more round.
+      } else if (player1points === player2points && player2Deck.length <= 3) {
+        war.unshift(player1Deck[0], player1Deck[1], player1Deck[2]);
+        war.unshift(player2Deck.length - 1);
+        player1Deck.splice(0, 3);
+        player2Deck.splice(0, player2Deck.length - 1);
+        console.log(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`);
+        //regular war if both players have enough cards
+      } else if (player1points === player2points) {
+        console.log(`Its War!, Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining`)
+        war.unshift(player1Deck[0], player1Deck[1], player1Deck[2]);
+        war.unshift(player2Deck[0], player2Deck[1], player2Deck[2]);
+        player1Deck.splice(0, 3);
+        player2Deck.splice(0, 3);
+        //player 1 wins the round, the cards are push in from the back in reverse or else there will be an infinite loop
+      } else if (player1points > player2points) {
+        console.log(`Player 1 wins this round. Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining and takes ${war.length} from the war and ${table.length} cards from the table`);
+        war.reverse();
+        player1Deck.push(...war);
+        war.length = 0;
+        table.reverse();
+        player1Deck.push(...table);
+        table.length = 0;
+        //player 2 wins the round. the cards go in from the front of the array as normal.
+      } else {
+        console.log(`Player 2 wins this round. Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining and takes ${war.length} from the war and ${table.length} cards from the table`);
+        player2Deck.push(...war);
+        war.length = 0;
+        player2Deck.push(...table);
+        table.length = 0;
+      }
+    }
+    //announce the winner
+    if (player1Deck == 0) {
+      console.log('Player 2 wins the game');
     } else {
-      console.log(`Player 2 wins this round. Player 1 has ${player1Deck.length} cards remaining and Player 2 has ${player2Deck.length} cards remaining and takes ${war.length} from the war and ${table.length} cards from the table`);
-      player2Deck.push(...war);
-      war.length = 0;
-      player2Deck.push(...table);
-      table.length = 0;
+      console.log('Player 1 wins the game');
     }
   }
-  //announce the winner
-  if (player1Deck == 0) {
-    console.log('Player 2 wins the game');
-  } else {
-    console.log('Player 1 wins the game');
-  }
 }
+
 //start the game function
-gameOfWar()
+let gameOfWar = new Setup
